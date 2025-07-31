c17bc26c23a3a8328c2753f9e9dc6ef56345878c
tonsojun <josephjun@gmail.com>
2025-07-31 11:04:17 -0700

    gnome war 3 prompt for weapon

--- my-code.js
+++ my-code.js
@@ -1,18 +1,65 @@
-//add prompt to ask how many tools they have. Convert the result to anumber using Number()
+/*
 
-//then add another prompt to ask how much food they're carrying.  Convet that one to a number too.
+1. Getting weapons ready for the soldiers
 
-//Next, create a variable called total that adds the number of tools and food.
+create a function called prepareWeapons() with a console.log inside that displays:  "Looks like you need your axe.  I'll get it ready for you."
 
-//Finally, show a summary in the console like this: "You have 8 items in total"
+and call that function in your code. 
 
-let toolsNumberString = prompt("How many tools do you have?");
-let toolsNumber = Number(toolsNumberString);
+*/
 
-let foodNumberString = prompt("How much food are you carrying?");
-let foodNumber = Number(foodNumberString);
+/*
+Displaying different weapons
 
-let total = (toolsNumber + foodNumber);
+update the previous function to accept an argument called weapon
 
-console.log("You have " + total + " items in total.");
+then use that weapon in the console lne like this: "Looks like you need your [weapon].  Ill get it ready for you!"
+"Looks like you need your sword. I'll get it ready for you!"
 
+*/
+
+/*
+
+now add a prompt() to ask the use what they need. 
+
+then call the function using what they typed in as the argument. 
+
+*/
+let weapon = prompt("What weapon do you need?")
+
+function prepareWeapons(weapon) {
+    console.log("Looks like you need your " + weapon + ".  I'll get it ready for you.");
+}
+
+prepareWeapons(weapon);
+
+
+/*
+
+Special preparation for battle hammers
+
+if the usre enters hammer, display the following lines:
+1. Battle hammer requested
+2. Factory notified.
+3. Your item will arrive in 3-5 business days. 
+
+If they enter anything else, show the usual mesage like before. 
+
+*/
+
+/*
+
+Moving hammer code into its own function
+
+create a separate function called prepareHammer()
+
+move the three hammer-related console.log() lines into that function
+
+then, instead of having those three lines inside prepareWeapons() just call prepareHammer()
+
+The code should still work the same:
+
+if the user asks for a hammer they should see those three lines
+
+if they ask for anything else they should see the usual message. 
+*/
