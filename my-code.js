d47cc7d331bf3e17b8f70065296093b52b416f70
tonsojun <josephjun@gmail.com>
2025-07-31 10:38:38 -0700

    completed gnome wars 2

--- my-code.js
+++ my-code.js
@@ -1,5 +1,18 @@
-let message = "What's your weapon?";
+//add prompt to ask how many tools they have. Convert the result to anumber using Number()
 
-let weapon_request = prompt(message);
+//then add another prompt to ask how much food they're carrying.  Convet that one to a number too.
 
-console.log(weapon_request);
+//Next, create a variable called total that adds the number of tools and food.
+
+//Finally, show a summary in the console like this: "You have 8 items in total"
+
+let toolsNumberString = prompt("How many tools do you have?");
+let toolsNumber = Number(toolsNumberString);
+
+let foodNumberString = prompt("How much food are you carrying?");
+let foodNumber = Number(foodNumberString);
+
+let total = (toolsNumber + foodNumber);
+
+console.log("You have " + total + " items in total.");
+
