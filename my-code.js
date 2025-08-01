0db4e9a0d8713318d85d2ff3af43d51460a62e85
tonsojun <josephjun@gmail.com>
2025-08-01 10:37:19 -0700

    completed build houses

--- my-code.js
+++ my-code.js
@@ -1,73 +1,25 @@
-/*
+let myButton = document.getElementById("help-button");
+myButton.addEventListener("click", buttonClicked);
 
-1. Getting weapons ready for the soldiers
+function buttonClicked() {
+    console.log("Thanks for your help, human!");
 
-create a function called prepareWeapons() with a console.log inside that displays:  "Looks like you need your axe.  I'll get it ready for you."
+    let newPElement = document.createElement("p");
+    newPElement.innerHTML = "This is a new paragraph element";
 
-and call that function in your code. 
+    document.body.appendChild(newPElement);
 
-*/
+    let newImageElement = document.createElement("img");
+    newImageElement.src = "gnome-warrior.webp";
 
-/*
-Displaying different weapons
+    document.body.appendChild(newImageElement);
 
-update the previous function to accept an argument called weapon
-
-then use that weapon in the console lne like this: "Looks like you need your [weapon].  Ill get it ready for you!"
-"Looks like you need your sword. I'll get it ready for you!"
-
-*/
-
-/*
-
-now add a prompt() to ask the use what they need. 
-
-then call the function using what they typed in as the argument. 
-
-*/
-
-/*
-
-Special preparation for battle hammers
-
-if the usre enters hammer, display the following lines:
-1. Battle hammer requested
-2. Factory notified.
-3. Your item will arrive in 3-5 business days. 
-
-If they enter anything else, show the usual mesage like before. 
-
-*/
-
-/*
-
-Moving hammer code into its own function
-
-create a separate function called prepareHammer()
-
-move the three hammer-related console.log() lines into that function
-
-then, instead of having those three lines inside prepareWeapons() just call prepareHammer()
-
-The code should still work the same:
-
-if the user asks for a hammer they should see those three lines
-
-if they ask for anything else they should see the usual message. 
-*/
-
-let weapon = prompt("What weapon do you need?")
-
-
-function prepareHammer(weapon) {
-    if(weapon === "Hammer") {
-        console.log("Battle hammer Requested");
-        console.log("Factory Notified");
-        console.log("Your item will arrive in 3-5 business days.")
-    } else 
-    console.log("Looks like you need your " + weapon + ".  I'll get it ready for you.");
 }
 
-prepareHammer(weapon);
+function buildHouses () {
+    for (let i = 0; i < 6; i++) {
+        console.log("House built!");
+    }
+}
 
-
+buildHouses()
